b1d9ac4c7616f1b63f5cd3e640e6821c9cd7dd94
Hemant-Kumar-jain <122785139+Hemant-Kumar-jain@users.noreply.github.com>
2023-02-08 02:43:59 -0800

    Add files via upload

---
+++ PrintFace.java
@@ -0,0 +1,12 @@
+package hemant.com;
+
+public class PrintFace {
+	public static void main(String[] args) {
+		System.out.println(" +'''''+ ");
+		System.out.println("[| o o |]");
+		System.out.println(" |  ^  | ");
+		System.out.println(" |  _  | ");
+		System.out.println(" +-----+");
+	}
+
+}
