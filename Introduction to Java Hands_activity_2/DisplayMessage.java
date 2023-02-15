fa5f0c0726ac9eb150202ff2cbbed06c09b787bf
Hemant-Kumar-jain <122785139+Hemant-Kumar-jain@users.noreply.github.com>
2023-02-15 02:44:35 -0800

    Add files via upload

---
+++ Introduction to Java Hands_activity_2/DisplayMessage.java
@@ -0,0 +1,18 @@
+package com.cognizant.shapes;
+
+class DisplayMessage{
+	 void printMessage(){
+		   System.out.println("Hello My Message");
+		}
+
+class  MyProgram
+{	
+	public static void main(String arg[])
+	{
+		DisplayMessage program=new DisplayMessage();
+		program.printMessage();
+	}
+}
+
+
+}
